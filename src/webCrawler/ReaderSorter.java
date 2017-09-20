b57b622e37c032f9581454b2f61597ba06759bce
Lt_Abstergo <azizmorshedkhan@gmail.com>
2017-09-20 17:07:33 -0400

    NDA

--- src/webCrawler/ReaderSorter.java
+++ src/webCrawler/ReaderSorter.java
@@ -5,20 +5,16 @@ import java.io.*;
 import java.util.Map;
 import java.util.Scanner;
 
-public class ReaderSorter
-{
-    public static void main(String[] args) throws FileNotFoundException
-    {
+public class ReaderSorter {
+    public static void main(String[] args) throws FileNotFoundException {
 
-        File file =new File( "/home/abstergo/" + JOptionPane.showInputDialog("insert file name"));
-        Scanner scanner= new Scanner(file);
-        while (scanner.hasNextLine())
-        {
-            String line=scanner.nextLine();
-            String[] part=line.split(" : ");
-            String[] partRoom=part[3].split(" ");
+        File file = new File("/home/abstergo/" + JOptionPane.showInputDialog("insert file name"));
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] part = line.split(" : ");
+            String[] partRoom = part[3].split(" ");
 
-            if()
         }
     }
 }
